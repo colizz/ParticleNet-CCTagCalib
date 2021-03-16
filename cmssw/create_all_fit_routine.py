951199848417a97941cccbc2d77be8e654b66fc8
Congqiao Li <congqiao.li@cern.ch>
2021-03-16 19:35:37 +0800

    Clean cmssw utils

--- cmssw/create_all_fit_routine.py
+++ cmssw/create_all_fit_routine.py
@@ -5,24 +5,42 @@ import glob
 import argparse
 parser = argparse.ArgumentParser('create all fit routine')
 parser.add_argument('--dir', default=None, help='Path to the base directory of ROOT output.')
-parser.add_argument('--full', action='store_true', help='Argument pass to write_cards_sf: to run the full fit routine including the implact.')
-parser.add_argument('--bdt', default='900', help='The BDT folder to run. Only in the routine for BDT varying validation we set --bdt 840,860,880,900,920,940')
+parser.add_argument('--ext-unce', default=None, help='Argument pass to write_cards_sf: set extra uncertainty term to run. e.g. --ext-unce NewTerm1,NewTerm2')
+parser.add_argument('--run-impact', action='store_true', help='Argument pass to write_cards_sf: to run impact plots.')
+parser.add_argument('--run-unce-breakdown', action='store_true', help='Argument pass to write_cards_sf: to run uncertainty breakdown')
+parser.add_argument('--bdt', default='900', help='The BDT folder to run. Set e.g. `--bdt 840,860,880,900,920,940` or `--bdt auto`.')
+parser.add_argument('-t', '--threads', type=int, default=8, help='Concurrent threads to run separate fits.')
 args = parser.parse_args()
 
 if not args.dir:
     raise RuntimeError('--dir is not provided!')
 
-abspath = os.path.abspath(os.getcwd())
+n_fits = 0
 str_runfit = ''
-for sam in glob.glob(args.dir):
-    for bdtval in args.bdt.split(','):
-        bdt = 'bdt'+bdtval
-        for pt in os.listdir(os.path.join(abspath, sam, 'Cards', bdt)):
-            inputdir = os.path.join(abspath, sam, 'Cards', bdt, pt)
-            cmd_runfit = '. write_cards_sf_wrapper.sh '+inputdir+(' full &' if args.full else ' &')
-            str_runfit += cmd_runfit+'\n'
-            print (cmd_runfit)
+ext_cmd = ''
+if args.ext_unce is not None:
+    ext_cmd += f'--ext-unce {args.ext_unce} '
+if args.run_impact:
+    ext_cmd += f'--run-impact '
+if args.run_unce_breakdown:
+    ext_cmd += f'--run-unce-breakdown '
+
+from utils import find_valid_runlist
+
+for inputdir in find_valid_runlist(args.dir, bdt_mode=args.bdt):
+    if not os.path.exists(inputdir):
+        raise RuntimeError('Input directory does not exists: ', inputdir)
+    n_fits += 1
+    cmd_runfit = f'python write_cards_sf.py {inputdir} {ext_cmd} &'
+
+    str_runfit += cmd_runfit+'\n'
     
+    if n_fits % args.threads == 0:
+        str_runfit += 'wait\n'
+str_runfit += 'wait\n'
+
+print (str_runfit)  
 with open('bg_runfit.sh', 'w') as fw:
     fw.write(str_runfit)
+
 print('File bg_runfit.sh is created.')
