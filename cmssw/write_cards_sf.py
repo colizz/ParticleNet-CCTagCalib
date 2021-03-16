657a3ab9bab30e6033861225c5bccd80fd42a218
Congqiao Li <congqiao.li@cern.ch>
2021-03-16 19:37:03 +0800

    Update cmssw utils: use single script write_cards_sf.py to implement fit

--- cmssw/write_cards_sf.py
+++ cmssw/write_cards_sf.py
@@ -7,10 +7,32 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(m
 
 import argparse
 parser = argparse.ArgumentParser('Preprocess ntuples')
-parser.add_argument('inputdir',
-    help='Input diretory.'
-)
+parser.add_argument('inputdir', help='Input diretory.')
+parser.add_argument('--ext-unce', default=None, help='Extra uncertainty term to run. e.g. --ext-unce NewTerm1,NewTerm2')
+parser.add_argument('--run-impact', action='store_true', help='Run impact plots.')
+parser.add_argument('--run-unce-breakdown', action='store_true', help='Run uncertainty breakdown')
 args = parser.parse_args()
+
+import yaml
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(dir_path, '../config.yml')) as f:
+    config = yaml.safe_load(f)
+
+if config['tagger']['type'].lower() == 'cc':
+    flv_poi1, flv_poi2 = 'flvC', 'flvB'
+elif config['tagger']['type'].lower() == 'bb':
+    flv_poi1, flv_poi2 = 'flvB', 'flvC'
+else:
+    raise RuntimeError('Tagger type in config.yml must be cc or bb.')
+
+import subprocess
+def runcmd(cmd):
+    """Run a shell command"""
+    p = subprocess.Popen(
+        cmd, shell=True, universal_newlines=True
+    )
+    out, _ = p.communicate()
+    return (out, p.returncode)
 
 cb = ch.CombineHarvester()
 cb.SetVerbosity(1)
@@ -29,7 +51,7 @@ cb.AddObservations(['*'], [''], ['13TeV'], [''], cats)
 bkg_procs = ['flvL']
 cb.AddProcesses(['*'], [''], ['13TeV'], [''], bkg_procs, cats, False)
 
-sig_procs = ['flvC', 'flvB']
+sig_procs = [flv_poi1, flv_poi2]
 cb.AddProcesses(['*'], [''], ['13TeV'], [''], sig_procs, cats, True)
 
 all_procs = bkg_procs + sig_procs
@@ -43,11 +65,15 @@ shapeSysts = {
     'fracLight':['flvL'],
 #     'qcdSyst':all_procs, # deprecated
 #     'qcdKdeSyst':all_procs, # deprecated
-#     'psWeightIsr':all_procs, # temporarily disabled for test
-#     'psWeightFsr':all_procs, # temporarily disabled for test
+    'psWeightIsr':all_procs,
+    'psWeightFsr':all_procs,
     'sfBDTRwgt':all_procs,
-    'sfBDTFloAround':all_procs,
+#     'sfBDTFloAround':all_procs, # deprecated since v2
+#     'fitVarRwgt':all_procs,
     }
+if args.ext_unce is not None:
+    for ext_unce in args.ext_unce.split(','):
+        shapeSysts[ext_unce] = all_procs
 
 for syst in shapeSysts:
     cb.cp().process(shapeSysts[syst]).AddSyst(cb, syst, 'shape', ch.SystMap()(1.0))
@@ -60,22 +86,22 @@ for dp, dn, filenames in os.walk(inputdir):
     if 'ignore' in dp:
         continue
     for f in filenames:
-        if f.endswith('.root'):
+        if f in ['inputs_pass.root', 'inputs_fail.root']:
             bin = f.replace('.root', '').replace('inputs_', '')
             if bin in bins:
                 fullpath = os.path.join(dp, f)
                 inputfiles[bin].append(fullpath)
 
 for bin in bins:
-    cmd = 'hadd -f {bin}.root {inputfiles}'.format(bin=bin, inputfiles=' '.join(inputfiles[bin]))
+    cmd = 'hadd -f {inputdir}/{bin}.root {inputfiles}'.format(inputdir=args.inputdir, bin=bin, inputfiles=' '.join(inputfiles[bin]))
     print(cmd)
-    os.system(cmd)
+    runcmd(cmd)
     cb.cp().bin([bin]).ExtractShapes(
-        '%s.root' % bin,
+        os.path.join(args.inputdir, '%s.root' % bin),
         '$PROCESS',
         '$PROCESS_$SYSTEMATIC'
         )
-    os.remove('%s.root' % bin)
+    os.remove(os.path.join(args.inputdir, '%s.root' % bin))
 
 if not useAutoMCStats:
     bbb = ch.BinByBinFactory()
@@ -83,18 +109,52 @@ if not useAutoMCStats:
     bbb.AddBinByBin(cb.cp().backgrounds(), cb)
 
 cb.PrintAll()
- 
-froot = ROOT.TFile('inputs_%s.root' % outputname.split('.')[0], 'RECREATE')
-cb.WriteDatacard(outputname + '.tmp', froot)
 
-with open(outputname, 'w') as fout:
-    with open(outputname + '.tmp') as f:
+froot = ROOT.TFile(os.path.join(args.inputdir, 'inputs_%s.root' % outputname.split('.')[0]), 'RECREATE')
+cb.WriteDatacard(os.path.join(args.inputdir, outputname + '.tmp'), froot)
+froot.Close()
+
+with open(os.path.join(args.inputdir, outputname), 'w') as fout:
+    with open(os.path.join(args.inputdir, outputname + '.tmp')) as f:
         for l in f:
             if 'rateParam' in l:
                 fout.write(l.replace('\n', '  [0.2,5]\n'))
             else:
                 fout.write(l)
-    os.remove(outputname + '.tmp')
+    os.remove(os.path.join(args.inputdir, outputname + '.tmp'))
 
     if useAutoMCStats:
         fout.write('* autoMCStats 20\n')
+
+## Start running higgs combine
+
+runcmd('''
+cd {inputdir} && \
+echo "+++ Converting datacard to workspace +++" && \
+text2workspace.py -m 125 -P HiggsAnalysis.CombinedLimit.TagAndProbeExtendedV2:tagAndProbe SF.txt --PO categories={flv_poi1},{flv_poi2},flvL && \
+echo "+++ Fitting... +++" && \
+combine -M MultiDimFit -m 125 SF.root --algo=singles --robustFit=1 | tee fit.log && \
+combine -M FitDiagnostics -m 125 SF.root --saveShapes --saveWithUncertainties --robustFit=1 > /dev/null 2>&1
+'''.format(inputdir=args.inputdir, flv_poi1=flv_poi1, flv_poi2=flv_poi2)
+)
+
+if args.run_impact:
+    runcmd('''
+cd {inputdir} && \
+combineTool.py -M Impacts -d SF.root -m 125 --doInitialFit --robustFit 1 >> pdf.log 2>&1 && \
+combineTool.py -M Impacts -d SF.root -m 125 --robustFit 1 --doFits >> pdf.log 2>&1 && \
+combineTool.py -M Impacts -d SF.root -m 125 -o impacts.json >> pdf.log 2>&1 && \
+plotImpacts.py -i impacts.json -o impacts >> pdf.log 2>&1
+'''.format(inputdir=args.inputdir)
+    )
+
+if args.run_unce_breakdown:
+    runcmd('''
+cd {inputdir} && \
+combine -M MultiDimFit -m 125 SF.root --algo=grid --robustFit 1 --points 50 -n Grid --redefineSignalPOIs SF_{flv_poi1} && \
+plot1DScan.py higgsCombineGrid.MultiDimFit.mH125.root --POI SF_{flv_poi1} && \
+combine -M MultiDimFit -m 125 SF.root --algo=singles --robustFit=1 -n Bestfit --saveWorkspace && \
+combine -M MultiDimFit -m 125 --algo=grid --points 50  -n Stat higgsCombineBestfit.MultiDimFit.mH125.root --redefineSignalPOIs SF_{flv_poi1} --snapshotName MultiDimFit --freezeParameters allConstrainedNuisances && \
+plot1DScan.py higgsCombineGrid.MultiDimFit.mH125.root --others 'higgsCombineStat.MultiDimFit.mH125.root:FreezeAll:2' --POI SF_{flv_poi1} -o unce_breakdown --breakdown Syst,Stat
+'''.format(inputdir=args.inputdir, flv_poi1=flv_poi1)
+    )
